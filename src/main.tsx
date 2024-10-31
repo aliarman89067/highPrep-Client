add43a4769d3b3d9c3198130bce01b787571ee90
ali arman <fiver89067@gmail.com>
2024-10-31 05:16:06 +0500

    Remove Loging

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,4 @@ import App from "./App.tsx";
 axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
 axios.defaults.withCredentials = true;
 
-console.log(import.meta.env.VITE_API_BASE_URL);
-
 createRoot(document.getElementById("root")!).render(<App />);
